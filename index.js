e79cfd01363aa06da11769bdf46b9b14c60de0b3
Murilo Vinícius de Faveri <124842759+MuhFaveri@users.noreply.github.com>
2023-11-20 22:23:30 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 //Declarando a função do express como variável
 const app = express();
 
+//Importação da conexão com a Data Base
+const connection = require('./mysql');
+
 //Definição da porta do servidor
 const port = 3000;
 
@@ -20,73 +23,79 @@ app.get('/', (req, res) => {
 });
 
 //------------------------------------------//
-
+    
 // Criando um array de clientes
 let clientes = [
-    { id: 1, nome: "Harry Potter", contato:999999 , endereco:"Rua dos Alfeneiros"},
-    { id: 2, nome: "Sirius Black", contato: 888888, endereco:"Rua da Cabana"},
+     
 ];
 
 // Rota para listar todos os clientes
 app.get("/clientes", (req, res) => {
-    console.log("get");
-    // Retorna uma lista de clientes
-    res.json(clientes);
+    const query = 'SELECT * FROM clientes';
+    connection.query(query, (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: "Erro ao buscar clientes" });
+            return;
+        }
+        res.json(results);
+    });
 });
+
 // Rota para obter um cliente por ID
 app.get("/clientes/:id", (req, res) => {
-    // Obtém o ID do cliente
     const id = parseInt(req.params.id);
-    // Procura o cliente no array
-    const cliente = clientes.find((cliente) => cliente.id === id);
-    // Retorna o cliente encontrado ou um erro
-    if (cliente) {
-        res.json(cliente);
-    } else {
-        res.status(404).json({ message: "Cliente não encontrado" });
-    }
+    const query = 'SELECT * FROM clientes WHERE id = ?';
+    connection.query(query, [id], (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: "Erro ao buscar cliente" });
+            return;
+        }
+        if (results.length > 0) {
+            res.json(results[0]);
+        } else {
+            res.status(404).json({ message: "Cliente não encontrado" });
+        }
+    });
 });
 
 // Rota para adicionar um novo cliente
-app.post("/cliente", (req, res) => {
-    // Obtém os dados do cliente
+app.post("/clientes", (req, res) => {
     const newCliente = req.body;
-    // Adiciona o cliente ao array
-    clientes.push(newCliente);
-    // Retorna o novo cliente
-    res.status(201).json(newCliente);
+    const query = 'INSERT INTO clientes SET ?';
+    connection.query(query, newCliente, (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: "Erro ao adicionar cliente" });
+            return;
+        }
+        res.status(201).json({ message: "Cliente adicionado com sucesso" });
+    });
 });
 
 // Rota para atualizar um cliente
-app.put("/cliente/:id", (req, res) => {
-    // Obtém o ID do cliente
+app.put("/clientes/:id", (req, res) => {
     const id = parseInt(req.params.id);
-    // Obtém os dados do cliente
     const updateCliente = req.body;
-    // Procura o cliente no array
-    const index = clientes.findIndex((cliente) => cliente.id === id);
-    // Atualiza o cliente encontrado ou retorna um erro
-    if (index !== -1) {
-        clientes[index] = { ...clientes[index], ...updateCliente };
-        res.json(clientes[index]);
-    } else {
-        res.status(404).json({ message: "Cliente não encontrado" });
-    }
+    const query = 'UPDATE clientes SET ? WHERE id = ?';
+    connection.query(query, [updateCliente, id], (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: "Erro ao atualizar cliente" });
+            return;
+        }
+        res.json({ message: "Cliente atualizado com sucesso" });
+    });
 });
 
 // Rota para remover um cliente
 app.delete("/clientes/:id", (req, res) => {
-    // Obtém o ID do cliente
     const id = parseInt(req.params.id);
-    // Procura o cliente no array
-    const index = clientes.findIndex((cliente) => cliente.id === id);
-    // Remove o cliente encontrado ou retorna um erro
-    if (index !== -1) {
-        const removeCliente = clientes.splice(index, 1);
-        res.json(removeCliente[0]);
-    } else {
-        res.status(404).json({ message: "Cliente não encontrado" });
-    }
+    const query = 'DELETE FROM clientes WHERE id = ?';
+    connection.query(query, [id], (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: "Erro ao remover cliente" });
+            return;
+        }
+        res.json({ message: "Cliente removido com sucesso" });
+    });
 });
 
 // Inicia o servidor
